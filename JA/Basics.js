16223770de4a4edb36959f294489bdc6731ca892
Sanyamhingar <sanyamhingar004@gmail.com>
2025-05-11 15:50:26 +0000

    learning day 3

--- JA/Basics.js
+++ JA/Basics.js
@@ -1,9 +1,14 @@
-"use strict" //treat all JS code as newer version
+//"use strict" //treat all JS code as newer version
 // alert(3+3) // we are using node js, not browser; pop-up notification
 
 let name = "Sanyam"
 let age = 14
 let isLoggedIn = false
 
+let ame =  null;
 
+//console.log(Name);
 
+console.log(typeof age)
+console.log(typeof ame); // output: Object
+console.log(typeof undefined); // output: undefined
