f1563c6fcfe45fb8d907a5d58a24dc5c8b373a11
Sanyamhingar <sanyamhingar004@gmail.com>
2025-05-11 16:15:39 +0000

    learning day 4

---
+++ JA/conversion.js
@@ -0,0 +1,29 @@
+// let score = 33
+
+// console.log(typeof score)
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber)
+
+// console.log(valueInNumber);
+
+// let score = null
+
+// console.log(typeof score)
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber)
+
+// console.log(valueInNumber);
+
+// 33 = 33
+// "33abc" = nan
+// true = 1
+
+// let isLoggedIn = 1
+
+// let booleanisloggedIn = Boolean(isLoggedIn)
+// console.log(booleanisloggedIn); //o/p = true
+
+let isloggedIn = "hitesh"
+
+let booleanisloggedIn = Boolean(isloggedIn)
+console.log(booleanisloggedIn); o/p = true
