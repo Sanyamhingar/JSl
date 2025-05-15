d11adfb458480bb5b6bbc9928b628806d2ba68f4
Sanyamhingar <sanyamhingar004@gmail.com>
2025-05-15 02:29:27 +0000

    learning day 6

--- JA/operations.js
+++ JA/operations.js
@@ -9,3 +9,33 @@ console.log(typeof stringNumber);
 let value = 3
 let negValue = -value
 console.log(negValue);
+console.log("1"+ 2) //12
+console.log(1 + "2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); // 32
+
+console.log((3+4)*5/3); // correct method to use
+
+
+console.log(+true); // 1
+//console.log(true+); // error
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); //101
+
+//********comparisons of datatypes*********
+console.log(null > 0); //false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+
+console.log(undefined == 0); // false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+// === (strict check)
