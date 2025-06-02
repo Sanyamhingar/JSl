18d8b8edf464b29c18ef3e9a9cc4c28d92eb4d55
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-02 03:17:16 +0000

    leanring day 7

--- JA/operations.js
+++ JA/operations.js
@@ -1,41 +1,50 @@
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-//**********Operations*******
+// //**********Operations*******
 
-let value = 3
-let negValue = -value
-console.log(negValue);
-console.log("1"+ 2) //12
-console.log(1 + "2"); //12
-console.log("1"+2+2); //122
-console.log(1+2+"2"); // 32
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log("1"+ 2) //12
+// console.log(1 + "2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); // 32
 
-console.log((3+4)*5/3); // correct method to use
-
-
-console.log(+true); // 1
-//console.log(true+); // error
-
-let gameCounter = 100;
-gameCounter++;
-console.log(gameCounter); // 101
-
-let gameCounter = 100;
-++gameCounter;
-console.log(gameCounter); //101
-
-//********comparisons of datatypes*********
-console.log(null > 0); //false
-console.log(null == 0); // false
-console.log(null >= 0); // true
+// console.log((3+4)*5/3); // correct method to use
 
 
-console.log(undefined == 0); // false
-console.log(undefined > 0);//false
-console.log(undefined < 0);//false
+// console.log(+true); // 1
+// //console.log(true+); // error
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter); // 101
+
+// let fgameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter); //101
+
+// //********comparisons of datatypes*********
+// console.log(null > 0); //false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
 
 // === (strict check)
+
+const id = Symbol("123")
+const anotherid = Symbol("123")
+
+console.log (id === anotherid);
+
+let bignumber = 789765465489754654n // by putting n in last number js autamatically detects as big integer
+
+// null ka type object hota h
