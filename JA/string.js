0ec97a14f1fe8ecdefd0ecadf44be493504046eb
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-12 04:58:23 +0000

    learning day 8

--- JA/string.js
+++ JA/string.js
@@ -7,4 +7,5 @@ age = 20;
 //console.log(name + age);
 
 //modern ways 
-console.log('hello my name is ${name} and my age is ${age}');
+console.log(`hello my name is ${name} and my age is ${age}`); // we use "backtick" key below esc to use variables  
+
