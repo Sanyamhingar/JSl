03cdab7b52ff401683661fbdd39c4568d4e2e336
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-13 05:06:43 +0000

    learning day 9

--- JA/string.js
+++ JA/string.js
@@ -9,3 +9,18 @@ age = 20;
 //modern ways 
 console.log(`hello my name is ${name} and my age is ${age}`); // we use "backtick" key below esc to use variables  
 
+const gamename = new String('sanyammm') //also a way
+
+console.log(gamename[0]);
+console.log(gamename.__proto__); // from this we can use various string feature
+
+//substring does not allows negative value 
+//slice allow using negative value and start iteration from backwards
+
+console.log(newString.trim()); // removes extra spaces from name or email which is not required
+
+
+const url = "https://sanyam.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar')) //false
