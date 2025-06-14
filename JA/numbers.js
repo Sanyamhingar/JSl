046dd0851bc23eb641f27863ec518c60e15fab83
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-14 03:22:56 +0000

    learning day 10

---
+++ JA/numbers.js
@@ -0,0 +1,23 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1)); //100.0
+// console.log(balance.toFixed(2));  // 100.00
+const otherNumber = 23.8966 
+
+console.log(otherNumber.toPrecision(2));
+
+const hundred = 10000000;
+console.log(hundred.toLocaleString('en-IN'));
+
+//********** Math**********
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4..6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
