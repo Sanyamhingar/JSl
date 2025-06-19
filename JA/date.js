0669f951bd200dcc366415bdefa427421397018f
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-19 04:27:51 +0000

    learning day 11

---
+++ JA/date.js
@@ -0,0 +1,17 @@
+//date is an object in  JS
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2025, 0, 19) // in this format month starts from 0
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2025, 0, 19, 5, 9)
+// console.log(myCreatedDate.toString());
+
+let myCreatedDate = new Date("19-06-2025") // in this format month starts from 01
+// console.log(myCreatedDate.toString());
+
