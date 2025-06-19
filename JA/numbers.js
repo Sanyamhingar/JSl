275d3a6ffa533a72eb3790e9e905aeb5591e8c23
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-19 03:56:56 +0000

    learning day 11

--- JA/numbers.js
+++ JA/numbers.js
@@ -6,18 +6,29 @@
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(1)); //100.0
 // console.log(balance.toFixed(2));  // 100.00
-const otherNumber = 23.8966 
+// const otherNumber = 23.8966
 
-console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(2));
 
-const hundred = 10000000;
-console.log(hundred.toLocaleString('en-IN'));
+// const hundred = 10000000;
+// console.log(hundred.toLocaleString('en-IN'));
 
 //********** Math**********
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4..6));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,5,6)); // same way we can find max
 
+// console.log(Math.random()); // it will print random numbers b/w 0 and 1
+
+// console.log((Math.random()*10)+1);// it will print randdom numbers bw 1 and 9 *usecase such as dice games.
+
+const max= 15;
+const min = 10;
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
+
+console.log(Math.floor(Math.random()));
