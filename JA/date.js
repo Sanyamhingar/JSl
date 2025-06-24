5d2d6984e25e394ab4ed69f4b18b15ac383795c5
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-24 03:33:58 +0000

    learning day 12

--- JA/date.js
+++ JA/date.js
@@ -12,6 +12,21 @@
 // let myCreatedDate = new Date(2025, 0, 19, 5, 9)
 // console.log(myCreatedDate.toString());
 
-let myCreatedDate = new Date("19-06-2025") // in this format month starts from 01
+// let myCreatedDate = new Date("19-06-2025") // in this format month starts from 01
 // console.log(myCreatedDate.toString());
 
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime);
+// console.log(Math.floor(Date.now/1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{ // ctrl and space to see suggestions
+    weekday: "long",
+    timeZone: '' 
+}
+)  
