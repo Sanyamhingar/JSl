5f2529227237345dedd9fb6519d03f20e5111b1e
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-24 04:07:03 +0000

    learning day 12

---
+++ JA/array.js
@@ -0,0 +1,10 @@
+// unshift is used to add element at the starting of the arrays
+// shift is used to remove the value from 0th index of the array
+
+let myArr = [0,1,2,3,4,5]
+const newArray = myArr.join()
+
+console.log(myArr);
+console.log(newArray);//converts array into string
+
+
