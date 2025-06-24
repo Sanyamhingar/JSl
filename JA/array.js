f4bc8e8d352401dcff9bb74f89b663f8037f71e4
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-24 04:16:27 +0000

    learning day 12

--- JA/array.js
+++ JA/array.js
@@ -2,9 +2,18 @@
 // shift is used to remove the value from 0th index of the array
 
 let myArr = [0,1,2,3,4,5]
-const newArray = myArr.join()
+// const newArray = myArr.join()
 
-console.log(myArr);
-console.log(newArray);//converts array into string
+// console.log(myArr);
+// console.log(newArray);//converts array into string
 
+// splice, slice
+
+console.log("A", myArr);
+console.log(myArr.splice(1,3)) // changes the whole array
+
+console.log("B", myArr);
+
+console.log(myArr.slice(1,3))
+console.log("c", myArr);
 
