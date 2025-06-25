ab6354b1368d215711e1b8f4ef6625a94da24b3a
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-25 04:22:12 +0000

    learning day 12

--- JA/array.js
+++ JA/array.js
@@ -1,7 +1,7 @@
 // unshift is used to add element at the starting of the arrays
 // shift is used to remove the value from 0th index of the array
 
-let myArr = [0,1,2,3,4,5]
+// let myArr = [0,1,2,3,4,5]
 // const newArray = myArr.join()
 
 // console.log(myArr);
@@ -9,11 +9,28 @@ let myArr = [0,1,2,3,4,5]
 
 // splice, slice
 
-console.log("A", myArr);
-console.log(myArr.splice(1,3)) // changes the whole array
+// console.log("A", myArr);
+// console.log(myArr.splice(1,3)) // changes the whole array
 
-console.log("B", myArr);
+// console.log("B", myArr);
 
-console.log(myArr.slice(1,3))
-console.log("c", myArr);
+// console.log(myArr.slice(1,3))
+// console.log("c", myArr);
+
+const Marvel_heros = ["Thor", "Loki", "Dr"];
+const DC_heros = ["Super", "Bat"];
+
+// Marvel_heros.push(DC_heros);
+// console.log(Marvel_heros); // it will create dc_heros as a sub array
+
+//console.log(Marvel_heros[3][1]); //to call the sub array 
+
+
+// console.log(Marvel_heros.push(DC_heros)); //this print new lenght of the array
+
+const newArray = Marvel_heros.concat(DC_heros)
+
+// console.log(newArray);
+
+// console.log(newArray[4]);
 
