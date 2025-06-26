fe990a5c327a4106eb4029137e455bec29b87975
Sanyamhingar <sanyamhingar004@gmail.com>
2025-06-26 02:36:09 +0000

    learning day 14

--- JA/objects.js
+++ JA/objects.js
@@ -1,8 +1,8 @@
-// Singleton- 'Constructors' se bnta h 
+// Singleton- 'Constructors' se bnta h
 //syntax - Object.create
-// multiple ways me bn skte h 'literals' se bnate h h toh 
+// multiple ways me bn skte h 'literals' se bnate h h toh
 
-const mysyl = Symbol("key1");
+const mysyl = Symbol('Key')
 
 const Jsuser = {
     name: "Snayam",
@@ -15,10 +15,49 @@ const Jsuser = {
 
 //**** Ways to call objects ****
 
-console.log(Jsuser.Email);
-console.log(Jsuser["fullname"]); // another method to call;
-console.log(Jsuser[mysyl]);
-console.log(typeof Jsuser[mysyl]);
+// console.log(Jsuser.Email);
+// console.log(Jsuser["fullname"]); // another method to call;
+// const keys = Object.getOwnPropertySymbols(Jsuser);
+// console.log(keys[0]);            // 👉 Symbol(Key)
+// console.log(typeof keys[0]);  
+//  console.log(Jsuser[mysyl]);
+//  console.log(typeof Jsuser[mysyl]);
+
+///***** change values in objects*********
+
+// Jsuser.Email = "Sanyamhingar@gmail.com"
+// Object.freeze(Jsuser)// after freezing we cannot implement any changes in the object
+// Jsuser.Email = "Sanyamjain@gmail.com"
+
+// console.log(Jsuser.Email);
+
+// Jsuser.greetings = function(){
+//     console.log("Hello JS user");
+// } 
+
+// console.log(Jsuser.greetings); //Output: [function(anonymus)] function execute nhi hua h bs function ka reference back aaya h
+// Jsuser.greetings(); // output: Hello JS user
+// console.log(Jsuser.greetings); // O/p : Undefined and hello...
+// *** Adding Multiple values
+Jsuser.greetings = function(){
+    console.log(`Hello, ${this.Age}`);
+
+}
+ Jsuser.greetings();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
