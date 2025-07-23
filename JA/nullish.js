6c35c99b06e2c3dae8ea467526ebaadddeeb4659
Sanyamhingar <sanyamhingar004@gmail.com>
2025-07-23 02:36:03 +0000

    efo

---
+++ JA/nullish.js
@@ -0,0 +1,16 @@
+// Nullish Coaleshing Operator
+
+let val1;
+// val1 = 5 ?? 10; //5
+val1 = null ?? 10; //10
+
+
+
+console.log(val1);  
+
+//Terniary Operator
+
+//Condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
