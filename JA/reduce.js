73d6ce1ae964befb822d0e9cc17a1b286cf65dc5
Sanyamhingar <sanyamhingar004@gmail.com>
2025-07-30 04:12:08 +0000

    dvf

--- JA/reduce.js
+++ JA/reduce.js
@@ -1,26 +1,35 @@
-//const myNumbers = [1,2,3,4,5,6,7,8,9]
+// //const myNumbers = [1,2,3,4,5,6,7,8,9]
 
-//const MyNums = myNumbers.map((num) => num+10)
-//console.log(MyNums);
+// //const MyNums = myNumbers.map((num) => num+10)
+// //console.log(MyNums);
 
-// const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// // const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// const myNums = [];
+// // const myNums = [];
 
-// myNumbers.forEach((num) => {
-//   if (num > 1) {
-//     myNums.push(num + 10);
-//   }
-// });
+// // myNumbers.forEach((num) => {
+// //   if (num > 1) {
+// //     myNums.push(num + 10);
+// //   }
+// // });
 
-// console.log(myNums);
+// // console.log(myNums);
 
-myArray = [1,2,3]
+// myArray = [1,2,3]
 
-//myNewArray = myArray.reduce((acc,curr) => acc + curr, 0)
-myNewArray = myArray.reduce(function (arr, curr) {
-  console.log(`val of acc is ${arr}, val of curr is ${curr}`)
-    return arr + curr
-})
+// //myNewArray = myArray.reduce((acc,curr) => acc + curr, 0)
+// myNewArray = myArray.reduce(function (arr, curr) {
+//   console.log(`val of acc is ${arr}, val of curr is ${curr}`)
+//     return arr + curr
+// })
 
-console.log(myNewArray)
+// // console.log(myNewArray)
+
+
+// const div = document.createElement("div")
+// // document.innerText = "Chai or code"// toh ye round trip bdhata h
+// // const addText = document.createTextNode("chai or code")
+// // div.appendChild(addText)
+
+// //document.body.appendChild(div)
+
